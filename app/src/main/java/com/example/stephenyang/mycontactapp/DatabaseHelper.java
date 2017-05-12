5834d545ae8b77e4b3951e2b066ae5e9a9e19dab
dschslava <stephenyang@stephens-mbp.sduhsd.lan>
2017-05-12 09:22:56 -0700

    Stephen Yang Update DatabaseHelper to create db
    added name field, ‘add’ button
    
    some niggling issues, to be resolved later

--- app/src/main/java/com/example/stephenyang/mycontactapp/DatabaseHelper.java
+++ app/src/main/java/com/example/stephenyang/mycontactapp/DatabaseHelper.java
@@ -2,7 +2,6 @@ package com.example.stephenyang.mycontactapp;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
@@ -10,24 +9,27 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DatabaseHelper extends SQLiteOpenHelper{
-
+    //purple variable names are those used
     public static final String DATABASE_NAME = "Contact.db";
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
     public static final String Col_2 = "NAME";
 
 
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db){
+        //in SQL
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
 
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 }
 
