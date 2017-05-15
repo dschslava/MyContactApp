96d201c08add7efe1042158defbad8ccfef128a0
dschslava <stephenyang@stephens-mbp.sduhsd.lan>
2017-05-15 08:58:50 -0700

    Code for inserting data into database
    done for name, pre-test code. toast messages added

--- app/src/main/java/com/example/stephenyang/mycontactapp/DatabaseHelper.java
+++ app/src/main/java/com/example/stephenyang/mycontactapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.stephenyang.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -13,7 +14,9 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String DATABASE_NAME = "Contact.db";
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
-    public static final String Col_2 = "NAME";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "PHONE NUMBER";
+    public static final String COL_4 = "ADDRESS";
 
 
     public DatabaseHelper(Context context) {
@@ -28,8 +31,24 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        //also in SQL
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, name);
+        contentValues.put(COL_4, name);
+
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == 1){
+            return false;
+        }
+        else return true;
     }
 }
 
--- app/src/main/java/com/example/stephenyang/mycontactapp/MainActivity.java
+++ app/src/main/java/com/example/stephenyang/mycontactapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.stephenyang.mycontactapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
@@ -15,5 +19,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper(this);
+    }
+
+    public void addData(View v){
+        boolean isInserted = myDb.insertData(editName.getText().toString());
+
+        if (isInserted){
+            Log.d("MyContact", "Data insertion successful");
+            //toast!!! data inserted correctly
+            Context context = getApplicationContext();
+            CharSequence text = "Contact added";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else{
+            Log.d("MyContact", "Data insertion unsuccessful");
+            //toast :( data inserted incorrectly
+            Context context = getApplicationContext();
+            CharSequence text = "Contact add unsuccessful";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 }
