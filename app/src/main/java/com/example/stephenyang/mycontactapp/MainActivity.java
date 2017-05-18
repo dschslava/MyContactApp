7f63a55ea6f1d369131d7a3951b29b97e413858b
dschslava <stephenyang@stephens-mbp.sduhsd.lan>
2017-05-18 09:05:31 -0700

    stephen yang search output cosmetics, function
    fix search output

--- app/src/main/java/com/example/stephenyang/mycontactapp/MainActivity.java
+++ app/src/main/java/com/example/stephenyang/mycontactapp/MainActivity.java
@@ -112,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                 if (res.getString(1).equals(search)) {
                     if(bufferCopy.indexOf(fields[0] + res.getString(0) + "\n") == -1) {
                         for (int field = 0; field < 4; field++) {
-                            if(field != 0) {buffer.append(fields[field] + res.getString(field) + "\n");}
+                            if(field != 0) {buffer.append(fields[field] + ": " + res.getString(field) + "\n");}
                             bufferCopy.append(fields[field] + res.getString(field) + "\n");
                         }
                         buffer.append("\n");
